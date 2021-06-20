a4c9f9ec16810ab945ac81633ce218bd7aa1712b
Zemliakov Konstantin <zemliakov.konstantin@gmail.com>
2021-06-20 22:42:22 +0600

    complete the first task

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,11 @@
+const isNumber = (value) => typeof value === 'number';
+
 function sum(a, b) {
-  /* ваш код */
+  if (!isNumber(a) || !isNumber(b)) {
+    throw new TypeError('Arguments must be numbers');
+  }
+
+  return a + b;
 }
 
 module.exports = sum;
