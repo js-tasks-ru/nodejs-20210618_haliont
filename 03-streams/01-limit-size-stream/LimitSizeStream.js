3115db5453b52736a1295245c85cdc8f519e9f43
Zemliakov Konstantin <zemliakov.konstantin@gmail.com>
2021-07-02 00:11:47 +0600

    complete limit size stream

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -4,10 +4,22 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+
+    this.limit = options.limit;
+    this.chunks = [];
   }
 
   _transform(chunk, encoding, callback) {
+    this.chunks.push(chunk);
+    const chunksByteLength = Buffer.concat(this.chunks).byteLength;
 
+    const isLimitExceeded = chunksByteLength > this.limit;
+
+    if (isLimitExceeded) {
+      callback(new LimitExceededError());
+    } else {
+      callback(null, chunk);
+    }
   }
 }
 
