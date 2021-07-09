b3d435ce4d4d6c8aae401aa50821db7b1ca16f02
Zemliakov Konstantin <zemliakov.konstantin@gmail.com>
2021-07-09 13:09:37 +0600

    complete line split stream

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,12 +4,29 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.lastLine = '';
   }
 
   _transform(chunk, encoding, callback) {
+    const string = this.lastLine + chunk.toString();
+    const lines = string.split(os.EOL);
+
+    this.lastLine = lines[lines.length - 1];
+
+    lines.slice(0, -1).forEach((line) => {
+      this.push(line);
+    });
+
+    callback();
   }
 
   _flush(callback) {
+    if (this.lastLine) {
+      this.push(this.lastLine);
+    }
+
+    this.lastLine = '';
+    callback();
   }
 }
 
