6ff3dd6c27d34b9e0a9ea67403061943f6b675c6
Zemliakov Konstantin <zemliakov.konstantin@gmail.com>
2021-07-21 00:12:51 +0600

    complete 06 01 task

--- 06-koajs/01-chat-app/app.js
+++ 06-koajs/01-chat-app/app.js
@@ -8,10 +8,39 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+let subscribers = {};
+
 router.get('/subscribe', async (ctx, next) => {
+  ctx.req.on('close', () => {
+    delete subscribers[id];
+  });
+
+  const id = Math.random();
+  const message = await new Promise((resolve) => {
+    subscribers[id] = resolve;
+  });
+
+  ctx.response.body = message;
 });
 
-router.post('/publish', async (ctx, next) => {
+const publishBodyValidation = (ctx, next) => {
+  if (!ctx.request.body.message) {
+    ctx.response.status = 400;
+    ctx.response.body = 'message field is required';
+  } else {
+    next();
+  }
+};
+
+router.post('/publish', publishBodyValidation, async (ctx, next) => {
+  const {message} = ctx.request.body;
+
+  Object.values(subscribers).forEach((resolveSubscriber) => {
+    resolveSubscriber(message);
+  });
+
+  subscribers = {};
+  ctx.response.status = 201;
 });
 
 app.use(router.routes());
